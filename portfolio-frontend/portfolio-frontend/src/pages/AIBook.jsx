9cb29798d9f6e9e6c411bba68af9263ef2e8bccd
Kevin Martinez <kevinm2020@icloud.com>
2026-03-27 12:20:57 -0500

    March 27 - 12:20 Working Version

--- portfolio-frontend/portfolio-frontend/src/pages/AIBook.jsx
+++ portfolio-frontend/portfolio-frontend/src/pages/AIBook.jsx
@@ -1,7 +1,9 @@
 function AIBook() {
   return (
     <div className="project-page">
-      <h1>AI Book</h1>
+      <h1>An introduction to AI Agents & Development</h1>
+      <h2>Written by: Kevin Martinez</h2>
+      <h3> A practical guide to building intelligent, autonomous systems trough a guide of a building my AI agent: Sonic</h3>
       <p className="status">✍️ In Progress</p>
 
       <p>
